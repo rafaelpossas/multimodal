c4dc84463e1150a535f11ae21b0e234ec513e83b
Rafael Possas <rafaelpossas@gmail.com>
2017-01-18 09:12:09 +1100

    Working Sparse Autoencoder

--- src/Autoencoders.py
+++ src/Autoencoders.py
@@ -1,174 +1,110 @@
-from keras.layers import Input, Dense, Activation
-from keras.models import Model, Sequential
+from keras.layers import Input, Dense
+from keras.models import Model
 from keras.datasets import mnist
+from keras.models import Model, Sequential
 from keras.utils.np_utils import to_categorical
 from keras import regularizers
 import numpy as np
-import matplotlib.pyplot as plt
+import softmax
 
-def ex_1():
-    # this is the size of our encoded representations
+# this is the size of our encoded representations
+encoding_dim = 196  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 
-    input_img = Input(shape=(784,))
-    encoded_1 = Dense(100, activation='relu')(input_img)
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
 
-    decoded = Dense(784, activation='relu')(encoded_1)
+# this model maps an input to its reconstruction
+autoencoder = Model(input=input_img, output=decoded)
 
-    # this model maps an input to its reconstruction
-    autoencoder = Model(input=input_img, output=decoded)
+# this model maps an input to its encoded representation
+encoder = Model(input=input_img, output=encoded)
 
-    encoder_1 = Model(input=input_img, output=encoded_1)
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
 
-    # create a placeholder for an encoded (32-dimensional) input
-    #encoded_input = Input(shape=(encoding_dim,))
-    # retrieve the last layer of the autoencoder model
-    #decoder_layer = autoencoder.layers[-1]
-    # create the decoder model
-    #decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
-
-    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    x_train = x_train.astype('float32') / 255.
-    x_test = x_test.astype('float32') / 255.
-    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-    print(x_train.shape)
-    print(x_test.shape)
-    print("Training the model...")
-    autoencoder.fit(x_train, x_train,
-                    nb_epoch=50,
-                    batch_size=256,
-                    shuffle=True,
-                    validation_data=(x_test, x_test),
-                    verbose=2)
-    print("Training Complete")
-    # encode and decode some digits
-    # note that we take them from the *test* set
-    #encoded_imgs = encoder.predict(x_test)
-    #decoded_imgs = decoder.predict(encoded_imgs)
-
-    # n = 10  # how many digits we will display
-    # plt.figure(figsize=(20, 4))
-    # for i in range(n):
-    #     # display original
-    #     ax = plt.subplot(2, n, i + 1)
-    #     plt.imshow(x_test[i].reshape(28, 28))
-    #     plt.gray()
-    #     ax.get_xaxis().set_visible(False)
-    #     ax.get_yaxis().set_visible(False)
-    #
-    #     # display reconstruction
-    #     ax = plt.subplot(2, n, i + 1 + n)
-    #     plt.imshow(decoded_imgs[i].reshape(28, 28))
-    #     plt.gray()
-    #     ax.get_xaxis().set_visible(False)
-    #     ax.get_yaxis().set_visible(False)
-    # plt.show()
-    my_w = autoencoder.layers[1].get_weights()
-    n = my_w[0].shape[1]  # how many digits we will display
-    plt.figure(figsize=(20, 10))
-    for i in range(n):
-        # display original
-        ax = plt.subplot(4, 8, i + 1)
-        plt.imshow(my_w[0].T[i].reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-    plt.show()
-
-def ex_2():
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-
-    input_img = Input(shape=(784,))
-    encoded = Dense(128, activation='relu')(input_img)
-    encoded = Dense(64, activation='relu')(encoded)
-    encoded = Dense(32, activation='relu')(encoded)
-
-    decoded = Dense(64, activation='relu')(encoded)
-    decoded = Dense(128, activation='relu')(decoded)
-    decoded = Dense(784, activation='sigmoid')(decoded)
-
-    autoencoder = Model(input=input_img, output=decoded)
-    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-
-    autoencoder.fit(x_train, x_train,
-                    nb_epoch=100,
-                    batch_size=256,
-                    shuffle=True,
-                    validation_data=(x_test, x_test))
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy',
+                    activity_regularizer=regularizers.activity_l1(10e-5))
 
 
-def ex_3():
-    hidden_size = 196
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    #x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-    #x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+unlabeled_train_index = np.argwhere(y_train >= 5)
+labeled_train_index = np.argwhere(y_train < 5)
 
-    unlabeled_train_index = np.argwhere(y_train >= 5)
-    labeled_train_index = np.argwhere(y_train < 5)
+num_train = round(labeled_train_index.shape[0] / 2)
 
-    labeled_test_index = np.argwhere(y_test < 5)
+train_index = labeled_train_index[0:num_train]
+test_index = labeled_train_index[num_train:]
 
-    unlabeled_data = x_train[unlabeled_train_index, :]
-    #unlabeled_data = unlabeled_data.astype('float32') / 255
-    unlabeled_data = unlabeled_data.reshape(len(unlabeled_data), np.prod(unlabeled_data.shape[1:]))
+unlabeled_data = x_train[unlabeled_train_index, :]
+unlabeled_data = unlabeled_data.astype('float32') / 255.
+unlabeled_data = unlabeled_data.reshape((len(unlabeled_data), np.prod(unlabeled_data.shape[1:])))
 
-    labeled_data_x = x_train[labeled_train_index, :]
-    #labeled_data_x = labeled_data_x.astype('float32') / 255
-    labeled_data_y = to_categorical(y_train[labeled_train_index])
+print x_train.shape
+print x_test.shape
 
-    labeled_data_x = labeled_data_x.reshape(len(labeled_data_x), np.prod(labeled_data_x.shape[1:]))
+autoencoder.fit(unlabeled_data, unlabeled_data,
+                nb_epoch=50,
+                batch_size=256,
+                shuffle=True)
 
-    x_test = x_test[labeled_test_index, :]
-    #x_test = x_test.astype('float32') / 255
-    x_test = x_test.reshape(len(x_test), np.prod(x_test.shape[1:]))
+labeled_train = x_train[train_index, :]
+labeled_train = labeled_train.astype('float32') / 255.
+labeled_train = labeled_train.reshape((len(labeled_train), np.prod(labeled_train.shape[1:])))
 
-    y_test = y_test[labeled_test_index]
-    y_test = to_categorical(y_test)
+labeled_train_y = y_train[train_index]
 
-    print('# examples in unlabeled set: {0:d}\n'.format(unlabeled_data.shape[0]))
-    print('# examples in supervised training set: {0:d}\n'.format(labeled_data_x.shape[0]))
+labeled_test = x_train[test_index, :]
+labeled_test = labeled_test.astype('float32') / 255.
+labeled_test = labeled_test.reshape((len(labeled_test), np.prod(labeled_test.shape[1:])))
+labeled_test_y = y_train[test_index]
 
-    input_img = Input(shape=(784,))
-    encoded = Dense(hidden_size, activation='relu',
-                    activity_regularizer=regularizers.activity_l1(10e-5))(input_img)
-    decoded = Dense(784, activation='sigmoid')(encoded)
+train_features = encoder.predict(labeled_train)
+test_features = encoder.predict(labeled_test)
 
-    autoencoder = Model(input=input_img, output=decoded)
-    encoder = Model(input=input_img, output=encoded)
+print(train_features.shape)
+print(test_features.shape)
 
-    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-    print("Training on Unlabeled data...")
-    autoencoder.fit(unlabeled_data, unlabeled_data,
-                    nb_epoch=50,
-                    batch_size=1000,
-                    shuffle=True,
-                    verbose=2)
-
-    train_features = encoder.predict(labeled_data_x)
-    test_features = encoder.predict(x_test)
-
-    softmax = Sequential()
-    softmax.add(Dense(input_dim=train_features.shape[1], output_dim=5, activation='sigmoid'))
-    softmax.add(Dense(output_dim=5, activation='softmax'))
-    softmax.compile(loss='categorical_crossentropy', optimizer='adadelta')
-
-    print("Training Softmax on labeled data...")
-    softmax.fit(train_features, labeled_data_y,
-                nb_epoch=100,
-                batch_size=1000,
-                shuffle=True,
-                verbose=2)
-
-    scores = softmax.evaluate(test_features, y_test, verbose=0)
-    print("Accuracy: {}".format(scores*100))
+# lambda_ = 1e-4
+# options_ = {'maxiter': 400, 'disp': True}
+#
+# tr_features = train_features.T
+# labeled_train_y = labeled_train_y.reshape(len(labeled_train_y))
+# opt_theta, input_size, num_classes = softmax.softmax_train(encoding_dim, 5,
+#                                                            lambda_, tr_features,
+#                                                            labeled_train_y, options_)
+#
+# ##======================================================================
+# ## STEP 5: Testing
+# ts_features = test_features.T
+# labeled_test_y = labeled_test_y.reshape(len(labeled_test_y))
+# predictions = softmax.softmax_predict((opt_theta, input_size, num_classes), ts_features)
+# print("Accuracy: {0:.2f}%".format(100 * np.sum(predictions == labeled_test_y, dtype=np.float64) / labeled_test_y.shape[0]))
 
 
 
-ex_3()
+softmax = Sequential()
+softmax.add(Dense(input_dim=train_features.shape[1], output_dim=5))
+softmax.add(Dense(output_dim=5, activation='softmax'))
+softmax.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+print("Training Softmax on labeled data...")
+softmax.fit(train_features, to_categorical(labeled_train_y),
+            nb_epoch=100,
+            batch_size=256,
+            verbose=2,
+            validation_split=0.1)
+pred = softmax.predict(test_features)
+scores = softmax.evaluate(test_features, to_categorical(labeled_test_y), verbose=0)
+print("Accuracy: {}".format(scores[1] * 100))
+# print("Accuracy: {0:.2f}%".format(100 * np.sum(np.argmax(pred, axis=1) == labeled_test_y.reshape(len(labeled_test_y)),
+#                                                dtype=np.float64) / labeled_test_y.shape[0]))
+
